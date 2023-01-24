a42076df650fe8844f40e55b42075bef33d44b3d
MiaChensSV <miachensv@gmail.com>
2023-01-24 09:52:18 +0100

    class of category and product
    class of category and product

--- OnlineShop_CourseSubmission_CS/Components/Category.cs
+++ OnlineShop_CourseSubmission_CS/Components/Category.cs
@@ -1,6 +1,10 @@
-﻿namespace OnlineShop_CourseSubmission_CS.Components
+﻿using OnlineShop_CourseSubmission_CS.Components;
+
+namespace OnlineShop_CourseSubmission_CS.Data
 {
     public class Category
     {
+        public string Name { get; set; }
+        public List<Products> Products { get; set; }
     }
 }
--- OnlineShop_CourseSubmission_CS/Components/Product.cs
+++ OnlineShop_CourseSubmission_CS/Components/Product.cs
@@ -1,6 +1,27 @@
-﻿namespace OnlineShop_CourseSubmission_CS.Components
+﻿namespace OnlineShop_CourseSubmission_CS.Data
 {
-    public class Product
+    public class Products
     {
+        public Products(int id, string title, decimal price, string description, Category category, string categoryName, string name, object rating)
+        {
+            this.id = id;
+            this.title = title;
+            this.price = price;
+            this.description = description;
+            this.category = category;
+            this.categoryName = categoryName;
+            this.rating = rating;
+
+        }
+        public int id { get; set; }
+        public string title { get; set; }
+
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public Category category { get; set; }
+        public string categoryName { get; set; }
+        public string image { get; set; }
+        public object rating { get; set; }
     }
+
 }
