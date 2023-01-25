5ed2a19458d7d92f013d559dc268aafc92494de1
MiaChensSV <miachensv@gmail.com>
2023-01-25 11:14:30 +0100

    productdetail page and fix some null return
    1,Create a product detail page.
    2, click on productImage, can be linked to detail of product detail.
    
    3, change some propertes' name to Capital letter in Products Class.
    4, fix some null value return

--- OnlineShop_CourseSubmission_CS/Components/Product.cs
+++ OnlineShop_CourseSubmission_CS/Components/Product.cs
@@ -1,16 +1,23 @@
-﻿namespace OnlineShop_CourseSubmission_CS.Components
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineShop_CourseSubmission_CS.Components
 {
     public class Products
     {
-        
-        public int id { get; set; }
-        public string title { get; set; }
-
-        public decimal price { get; set; }
-        public string description { get; set; }
-        public string category { get; set; }
-        public string image { get; set; }
-        public object rating { get; set; }
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+		[JsonPropertyName("price")]
+		public decimal Price { get; set; }
+		[JsonPropertyName("description")]
+		public string? Description { get; set; }
+		[JsonPropertyName("category")]
+		public string? Category { get; set; }
+		[JsonPropertyName("image")]
+		public string? Image { get; set; }
+		[JsonPropertyName("rating")]
+		public Rating? Rating { get; set; }
     }
 
 }
--- OnlineShop_CourseSubmission_CS/Program.cs
+++ OnlineShop_CourseSubmission_CS/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<Data>();
+builder.Services.AddSingleton<DataService>();
 
 await builder.Build().RunAsync();
