aaf0162ec692f69b68c402dc296142180df2508b
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2023-01-30 15:36:45 +0100

    first test pairprogramming

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,9 +1,54 @@
+using OnlineShop_CourseSubmission_CS;
+using OnlineShop_CourseSubmission_CS.Components;
+using OnlineShop_CourseSubmission_CS.Data;
+
 namespace UnitTests
 {
     public class UnitTest1
     {
+
+
+        public static List<CartItem> CartList = new();
+
+        [Theory]
+        [InlineData(112)]
+        public static async Task AddToCart(int productId)
+            {
+                DataService data = new DataService();
+                Products[] allProd = await data.GetAllProducts();
+                Products product = allProd.FirstOrDefault(x => x.Id == productId);
+                CartItem? foundCartItem = CartList.Find(cartItem => cartItem.Product.Id == productId);
+                if (foundCartItem == null)
+                {
+                    CartItem newCartItem = new CartItem()
+                    {
+                        UserId = 1,
+                        CartId = 1,
+                        Product = product,
+                        Quantity = 1
+                    };
+                    CartList.Add(newCartItem);
+                }
+                else
+                {
+                    foundCartItem.Quantity++;
+                }
+            }
+
         [Fact]
-        public void Test1()
+        public void UpdateTotalPrice()
+        {
+
+        }
+
+        [Fact]
+        public void UpdateQuantity()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteProduct()
         {
 
         }
