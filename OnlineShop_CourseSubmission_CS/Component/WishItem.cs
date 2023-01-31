c85bb00f2dd93bf0adb741d1d3982e5757714e0a
MiaChensSV <miachensv@gmail.com>
2023-01-31 12:21:04 +0100

    add event to directing More Info page and add event to function Addto WishList. In wishlist page, render out information of products.
    add event to directing More Info page and add event to function Addto WishList.
    In wishlist page, render out information of products.

---
+++ OnlineShop_CourseSubmission_CS/Component/WishItem.cs
@@ -0,0 +1,27 @@
+﻿using OnlineShop_CourseSubmission_CS.Components;
+using OnlineShop_CourseSubmission_CS.Data;
+
+namespace OnlineShop_CourseSubmission_CS.Component
+{
+	public class WishItem
+	{
+		public static List<Products> WishList = new();
+		public static async void addtoWish(int productId)
+		{
+			DataService data = new DataService();
+			Products[] allProd = await data.GetAllProducts();
+			Products? product = allProd.FirstOrDefault(x => x.Id == productId);
+
+			Products prodWish=WishList.Find(x=>x.Id==productId);
+			if (prodWish != null)
+			{
+				return;
+			}
+			else
+			{
+				Products addedWish = product;
+				WishList.Add(addedWish);
+			}
+		}
+	}
+}
