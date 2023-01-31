e28e1d9793e7d162503bafe2952642e896b98dc3
MiaChensSV <miachensv@gmail.com>
2023-01-31 11:14:37 +0100

    add function and Test of Delete.
    add function and Test of Delete.

--- UnitTests/ShoppingCartTest.cs
+++ UnitTests/ShoppingCartTest.cs
@@ -1,3 +1,4 @@
+using System.Net.Security;
 using OnlineShop_CourseSubmission_CS;
 using OnlineShop_CourseSubmission_CS.Component;
 using OnlineShop_CourseSubmission_CS.Components;
@@ -5,13 +6,13 @@ using OnlineShop_CourseSubmission_CS.Data;
 
 namespace UnitTests
 {
-    public class UnitTest1
+    public class ShoppingCartTest
     {
 
         [Fact]
         public async void TestAdd()
         {
-
+            ShoppingCart.Empty();
             Products prod1 = new Products()
             {
                 Id = 1,
@@ -55,6 +56,7 @@ namespace UnitTests
         [Fact]
         public async void UpdateCart()
         {
+            ShoppingCart.Empty();
             Products prod1 = new Products()
             {
                 Id = 1,
@@ -93,12 +95,49 @@ namespace UnitTests
             Assert.Equal(3, ShoppingCart.TotalQuantity);
 
         }
-
         
-
         [Fact]
         public void DeleteProduct()
         {
+            ShoppingCart.Empty();
+            Products prod1 = new Products()
+            {
+                Id = 1,
+                Category = "new cata",
+                Price = 100,
+                Title = "Prod 1",
+                Description = "",
+                Image = ""
+            };
+
+            Products prod2 = new Products()
+            {
+                Id = 2,
+                Category = "new cata",
+                Price = 100,
+                Title = "Prod 2",
+                Description = "",
+                Image = ""
+            };
+            CartItem item1 = new CartItem()
+            {
+                CartId = 1,
+                Product = prod1,
+                Quantity = 1,
+            };
+            CartItem item2 = new CartItem()
+            {
+                CartId = 2,
+                Product = prod2,
+                Quantity = 1,
+            };
+            ShoppingCart.CartList = new List<CartItem>() { item1, item2 };
+
+            ShoppingCart.Delete(1);
+
+            Assert.Single(ShoppingCart.CartList);
+            Assert.Equal(1, ShoppingCart.TotalQuantity);
+            Assert.Equal(100, ShoppingCart.TotalPrice);
 
         }
     }
