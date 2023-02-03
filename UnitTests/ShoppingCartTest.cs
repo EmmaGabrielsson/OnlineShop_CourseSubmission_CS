d09be1b2fdc210de0fed1c6fd5b5c5626158ecf9
MiaChensSV <miachensv@gmail.com>
2023-02-03 15:22:06 +0100

    add comments
    add comments

--- UnitTests/ShoppingCartTest.cs
+++ UnitTests/ShoppingCartTest.cs
@@ -22,7 +22,6 @@ namespace UnitTests
                 Description = "",
                 Image = ""
             };
-
             Products prod2 = new Products()
             {
                 Id = 2,
@@ -32,15 +31,17 @@ namespace UnitTests
                 Description = "",
                 Image = ""
             };
-
             DataStorage.ProductList= new Products[] { prod1,prod2 };
 
-
+            //test add prod1 to shoppingcart
             await ShoppingCart.AddToCart(1);
+            //check only one product is in shoppingcart
             Assert.Single(ShoppingCart.CartList);
+            //add prod1 again
             await ShoppingCart.AddToCart(1);
-            Assert.Single(ShoppingCart.CartList);
-
+			//check only one product is in shoppingcart
+			Assert.Single(ShoppingCart.CartList);
+            //check if prod1 is in shoppingcart
             CartItem? cartItem = ShoppingCart.CartList.FirstOrDefault((item) =>
             {
                 if (item.Product != null)
@@ -51,11 +52,13 @@ namespace UnitTests
 
             });
             Assert.NotNull(cartItem);
+            //check prod1's quantity =2
             Assert.Equal(2, cartItem.Quantity);
-
-            await ShoppingCart.AddToCart(3);
+			
+            //test add prod3 to shoppingcart
+			await ShoppingCart.AddToCart(3);
             Assert.Single(ShoppingCart.CartList);
-
+            //try to find if prod3 exist, if not exist, retrun false, there is no prod3 in cartItemList
             CartItem? cartItem3 = ShoppingCart.CartList.FirstOrDefault((item) =>
             {
                 if (item.Product != null)
@@ -64,9 +67,8 @@ namespace UnitTests
                 }else
                     return false;
             });
-          
-            Assert.Null(cartItem3);
 
+            Assert.Null(cartItem3);
         }
         
         [Fact]
@@ -105,8 +107,9 @@ namespace UnitTests
                 Quantity = 1,
             };
             ShoppingCart.CartList = new List<CartItem>() { item1, item2 };
-
+            //add prod1 to cart
             await ShoppingCart.AddToCart(1);
+            //prod1 is already in list, quantity of prod1++, prod2 quantit1, total quantity=3, total price=305, total price exclude shipping=300
             Assert.Equal(300, ShoppingCart.TotalExShipping);
             Assert.Equal(305, ShoppingCart.TotalPrice);
             Assert.Equal(3, ShoppingCart.TotalQuantity);
@@ -149,9 +152,9 @@ namespace UnitTests
                 Quantity = 1,
             };
             ShoppingCart.CartList = new List<CartItem>() { item1, item2 };
-
+            //delete prod1
             ShoppingCart.Delete(1);
-
+            //had 2 products in shopping cart, after delete, only one product in shopping cart. total quantity=1,totalExshipping=100.totoal pris=105
             Assert.Single(ShoppingCart.CartList);
             Assert.Equal(1, ShoppingCart.TotalQuantity);
             Assert.Equal(100, ShoppingCart.TotalExShipping);
